20e0d9b5cf5367388b73481d5c9fabaf6041a0fd
descart <descart@mail.uni-paderborn.de>
2017-12-15 13:21:17 +0100

    Fix CategoryWithPullbacks

--- FMDE/src/org/upb/fmde/miniproject/delta/CategoryWithPullbacks.java
+++ FMDE/src/org/upb/fmde/miniproject/delta/CategoryWithPullbacks.java
@@ -1,33 +1,33 @@
 package org.upb.fmde.miniproject.delta;
 
 import org.upb.fmde.de.categories.Category;
-import org.upb.fmde.de.categories.colimits.CoLimit;
+import org.upb.fmde.de.categories.colimits.pushouts.CoSpan;
 import org.upb.fmde.de.categories.colimits.pushouts.Span;
+import org.upb.fmde.de.categories.limits.Limit;
 
 public interface CategoryWithPullbacks<Ob, Arr> extends Category<Ob, Arr> {
-
-default CoLimit<Span<Arr>, Arr> pullback(Span<Arr> span) {
-		Ob B = source(span.left);
-		Ob C = source(span.right);
+	
+	default Limit<Span<Arr>, Arr> pullback(CoSpan<Arr> cospan) {
+		Ob R = source(cospan.right);
+		Ob G = source(cospan.left);
 		
-		CoLimit<Span<Arr>, Arr> product = product(B, C);
+		Limit<Span<Arr>, Arr> product = product(R, G);
 		
-		Arr piC = product.obj.left;
-		Arr piB = product.obj.right;
-		CoLimit<Arr, Arr> equaliser = equaliser(
-				compose(piC, span.right),
-				compose(piB, span.left));
+		Limit<Arr, Arr> equaliser = equaliser(
+				compose(product.obj.left, cospan.right),
+				compose(product.obj.right, cospan.left));
 		
-		return new CoLimit<>(
+		return new Limit<Span<Arr>, Arr> (
 				new Span<Arr>(this,
-				compose(equaliser.obj, piC),
-				compose(equaliser.obj, piB)),
-				spanofarrows -> {
-					Arr a = product.up.apply(spanofarrows);
+				compose(equaliser.obj, product.obj.left),
+				compose(equaliser.obj, product.obj.right)),
+				span -> {
+					Arr a = product.up.apply(span);
 					return equaliser.up.apply(a);
 				});
 	}
-CoLimit<Arr, Arr> equaliser(Arr f, Arr g);
-
-CoLimit<Span<Arr>, Arr> product(Ob a, Ob b);
+	
+	Limit<Arr, Arr> equaliser(Arr f, Arr g);
+	
+	Limit<Span<Arr>, Arr> product(Ob a, Ob b);
 }
