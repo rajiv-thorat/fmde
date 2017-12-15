a56fffabb22e31d8c7083b4ef24e476c791e810a
descart <descart@mail.uni-paderborn.de>
2017-12-15 13:46:37 +0100

    Implement product initially.

--- FMDE/src/org/upb/fmde/miniproject/delta/FinSetsWithPullbacks.java
+++ FMDE/src/org/upb/fmde/miniproject/delta/FinSetsWithPullbacks.java
@@ -1,5 +1,9 @@
 package org.upb.fmde.miniproject.delta;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.upb.fmde.de.categories.colimits.pushouts.Span;
 import org.upb.fmde.de.categories.concrete.finsets.FinSet;
 import org.upb.fmde.de.categories.concrete.finsets.FinSets;
@@ -22,7 +26,15 @@ public class FinSetsWithPullbacks extends FinSets implements CategoryWithPullbac
 
 	@Override
 	public Limit<Span<TotalFunction>, TotalFunction> product(FinSet a, FinSet b) {
-		// TODO Auto-generated method stub
+		Map<Object, Object> hm = new HashMap<>();
+		TotalFunction piG = new TotalFunction(b, null, b);
+		a.elts().forEach(elementA->{
+			b.elts().forEach(elementB->{
+				hm.put(elementA, elementB);
+			});
+		});
+		FinSet product = new FinSet("cartesian product", hm.entrySet().stream().collect(Collectors.toList()));
+		
 		return null;
 	}
 
