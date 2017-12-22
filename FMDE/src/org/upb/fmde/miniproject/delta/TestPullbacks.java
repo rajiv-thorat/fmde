d8213b57103aaadad345a7d99eb02bb95417c5fa
Rajiv Thorat <rajivthorat2003@gmail.com>
2017-12-22 13:28:58 +0100

    NEw test.

--- FMDE/src/org/upb/fmde/miniproject/delta/TestPullbacks.java
+++ FMDE/src/org/upb/fmde/miniproject/delta/TestPullbacks.java
@@ -15,10 +15,21 @@ import org.junit.Test;
 import org.upb.fmde.de.categories.colimits.pushouts.Span;
 import org.upb.fmde.de.categories.concrete.finsets.FinSet;
 import org.upb.fmde.de.categories.concrete.finsets.FinSetDiagram;
+import org.upb.fmde.de.categories.concrete.finsets.FinSets;
 import org.upb.fmde.de.categories.concrete.finsets.TotalFunction;
 import org.upb.fmde.de.categories.diagrams.Diagram;
 import org.upb.fmde.de.categories.limits.Limit;
+import org.upb.fmde.de.tests.TestUtil;
 public class TestPullbacks {
+	
+	private static final String diagrams = "diagrams/miniproj/";
+	
+	
+	@BeforeClass
+	public static void clear() {
+		TestUtil.clear(diagrams);
+	}
+	
 	@Test
 	public void testProductEmptyUnivProperty() {
 		Diagram<FinSet, TotalFunction> d1 = createDiagram1();
@@ -52,6 +63,58 @@ public class TestPullbacks {
 		System.out.println("univ_prop empty small");
 		printMap(univResultSmaller.mappings());
 		assertTrue(univResultSmaller.mappings().get(simpleEntry).equals(simpleEntry));
+	}
+	
+	@Test
+	public void testUnivProp() throws IOException {
+		Diagram<FinSet, TotalFunction> diag = createDiagram2();
+		FinSetsWithPullbacks fwp = new FinSetsWithPullbacks();
+		Limit<Span<TotalFunction>, TotalFunction> product = fwp.product(diag.getObject("G"), diag.getObject("R"));
+		
+		
+		
+		
+		List<Object> listofEntries = new ArrayList<>();
+		
+		// this is the actual candidate.
+		AbstractMap.SimpleEntry<Object, Object> entry1 = new AbstractMap.SimpleEntry<Object, Object>(diag.getObject("G").elts().get(0), diag.getObject("R").elts().get(0));
+		AbstractMap.SimpleEntry<Object, Object> entry2 = new AbstractMap.SimpleEntry<Object, Object>(diag.getObject("G").elts().get(1), diag.getObject("R").elts().get(0));
+		
+		listofEntries.add(entry1);
+		listofEntries.add(entry2);
+		
+		FinSet candidate = new FinSet("candidate", listofEntries);
+		
+		TotalFunction univPropTest3 = new TotalFunction(candidate, "candidate_G", diag.getObject("G"));
+		TotalFunction univPropTest4 = new TotalFunction(candidate, "candidate_R", diag.getObject("R"));
+				
+		univPropTest3.addMapping(entry1,diag.getObject("G").elts().get(0));
+		univPropTest4.addMapping(entry1,diag.getObject("R").elts().get(0));
+		univPropTest3.addMapping(entry2,diag.getObject("G").elts().get(1));
+		univPropTest4.addMapping(entry2,diag.getObject("R").elts().get(0));		
+		
+		
+		
+		TotalFunction univResultSmaller = product.up.apply(new Span<TotalFunction>(FinSetsWithPullbacks.FinSets, univPropTest3, univPropTest4));
+		System.out.println("univ_prop empty candidate");
+		printMap(univResultSmaller.mappings());
+		
+		
+		
+		Diagram<FinSet, TotalFunction> diagToShow = new FinSetDiagram();
+		
+		diagToShow.objects(diag.getObject("G"),diag.getObject("R"), product.obj.left.trg(), candidate).arrows(product.obj.left,product.obj.right, univPropTest3, univPropTest4, univResultSmaller, FinSets.FinSets.id(candidate));
+		
+		
+		
+		
+		diagToShow.saveAsDot(diagrams, "idcheck")
+		 .prettyPrint(diagrams, "idcheck");
+		
+		assertTrue(FinSets.FinSets.id(candidate).isTheSameAs(univResultSmaller));
+		
+		
+		
 	}
 	@Test
 	public void testProductResultnotEmpty() {
@@ -106,5 +169,21 @@ public class TestPullbacks {
 
 		return d1;
 	}
+	
+	private FinSetDiagram createDiagram2() {
+		FinSet G = new FinSet("G", "a", "b");
+		FinSet R = new FinSet("R", 3);
+		
+		TotalFunction g = new TotalFunction(G, "f", R)
+				.addMapping(G.get("a"), R.get("3"))
+				.addMapping(G.get("b"), R.get("3"));
+				
+		FinSetDiagram d1 = new FinSetDiagram();
+		d1.objects(G, R).arrows(g);
+
+		return d1;
+	}
+	
+	
 
 }
