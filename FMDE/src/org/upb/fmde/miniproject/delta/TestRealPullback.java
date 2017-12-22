eb54f9009d6fae4f6e0e97586ead583a8ac9dd0b
Prenglis <j.e@vollbio.de>
2017-12-22 11:31:52 +0100

    add test for pullback

---
+++ FMDE/src/org/upb/fmde/miniproject/delta/TestRealPullback.java
@@ -0,0 +1,64 @@
+package org.upb.fmde.miniproject.delta;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+import java.io.IOException;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.upb.fmde.de.categories.colimits.pushouts.CoSpan;
+import org.upb.fmde.de.categories.colimits.pushouts.Span;
+import org.upb.fmde.de.categories.concrete.finsets.FinSet;
+import org.upb.fmde.de.categories.concrete.finsets.FinSetDiagram;
+import org.upb.fmde.de.categories.concrete.finsets.TotalFunction;
+import org.upb.fmde.de.categories.diagrams.Diagram;
+import org.upb.fmde.de.categories.limits.Limit;
+public class TestRealPullback {
+
+	@Test
+	public void testEqualiserUniversality() {
+		FinSetDiagram d1 = createDiagram1();
+//		FinSet fsB =  d1.getObject("B");
+//		FinSet fsA =  d1.getObject("A");
+//		FinSet fsC =  d1.getObject("C");
+		TotalFunction tfDPrime = d1.getArrow("d'"); 
+		TotalFunction tfBPrime = d1.getArrow("b'");
+		FinSetsWithPullbacks fwp = new FinSetsWithPullbacks();
+		Limit<Span<TotalFunction>, TotalFunction> limitPullback = fwp.pullback(new CoSpan<TotalFunction>(fwp.FinSets, tfBPrime, tfDPrime));
+		//FinSet fsDPrime = new FinSet("D'", )
+	}
+	@Test
+	public void testPullback() {
+		FinSetDiagram d1 = createDiagram1();
+//		FinSet fsB =  d1.getObject("B");
+//		FinSet fsA =  d1.getObject("A");
+//		FinSet fsC =  d1.getObject("C");
+		TotalFunction tfDPrime = d1.getArrow("d'"); 
+		TotalFunction tfBPrime = d1.getArrow("b'");
+		FinSetsWithPullbacks fwp = new FinSetsWithPullbacks();
+		Limit<Span<TotalFunction>, TotalFunction> limitPullback = fwp.pullback(new CoSpan<TotalFunction>(fwp.FinSets, tfBPrime, tfDPrime));
+		assertTrue(limitPullback.obj.left.src().elts().contains("P_B"));
+		
+	}
+	private FinSetDiagram createDiagram1() {
+		FinSet fsB = new FinSet("B", "P_B", "I_B");
+		FinSet fsA = new FinSet("A", "X_A", "C_A", "D_A");
+		FinSet fsC = new FinSet("C", "P_C", "A_C");
+		TotalFunction tfDPrime = new TotalFunction(fsB, "d'", fsA); 
+		TotalFunction tfBPrime = new TotalFunction(fsC, "b'", fsA);
+		tfDPrime.addMapping(fsB.get("P_B"), fsA.get("X_A"));
+		tfDPrime.addMapping(fsB.get("I_B"), fsA.get("D_A"));
+		tfBPrime.addMapping(fsC.get("P_C"), fsA.get("C_A"));
+		tfBPrime.addMapping(fsC.get("A_C"), fsA.get("X_A"));
+		FinSetDiagram d1 = new FinSetDiagram();
+		d1.objects(fsB, fsA, fsC).arrows(tfDPrime, tfBPrime);
+
+		return d1;
+	}
+}
+
