91de434d496d99ec11cfa7be02ff1df73f8cd8fc
Prenglis <j.e@vollbio.de>
2018-01-19 13:20:29 +0100

    update Delta

--- FMDE/src/org/upb/fmde/miniproject/delta/DeltaCategory.java
+++ FMDE/src/org/upb/fmde/miniproject/delta/DeltaCategory.java
@@ -5,15 +5,48 @@ import org.upb.fmde.de.categories.colimits.pushouts.CoSpan;
 import org.upb.fmde.de.categories.colimits.pushouts.Span;
 import org.upb.fmde.de.categories.limits.Limit;
 
-public interface DeltaCategory<Ob, Arr> extends Category<Ob, Arr>{
+public class DeltaCategory<Ob, Arr> implements Category<Ob, Span<Arr>>{
+	protected CategoryWithPullbacks<Ob, Arr> cat;
 
-	default public Span<Arr> deltaCompose(CategoryWithPullbacks<Ob, Arr> cwb, Span<Arr> deltaF, Span<Arr> deltaG) {
-		 Limit<Span<Arr>, Arr> pullback = cwb.pullback(new CoSpan<Arr>(cwb, deltaF.right , deltaG.left));
+	public DeltaCategory(CategoryWithPullbacks<Ob, Arr> pCat) {
+		this.cat = pCat;
+	}
+	@Override
+	public Span<Arr> compose(Span<Arr> f, Span<Arr> g) {
+		 Limit<Span<Arr>, Arr> pullback = cat.pullback(new CoSpan<Arr>(cat, 
+				 f.right , g.left));
 		 Arr left = pullback.obj.left;
 		 Arr right = pullback.obj.right;
-		 Arr leftDeltaFLeft = compose(left, deltaF.left);
-		 Arr rightDeltaGRight = compose(right, deltaG.right);
-		 return new Span(cwb, leftDeltaFLeft, rightDeltaGRight);
+		 Arr leftDeltaFLeft = cat.compose(left, f.left);
+		 Arr rightDeltaGRight = cat.compose(right, g.right);
+		 return new Span<Arr>(cat, leftDeltaFLeft, rightDeltaGRight);
 	}
-	
+
+	@Override
+	public Span<Arr> id(Ob o) {
+		return new Span<Arr>(cat, cat.id(o), cat.id(o));
+	}
+
+	@Override
+	public Ob source(Span<Arr> f) {
+		return cat.target(f.left);
+	}
+
+	@Override
+	public Ob target(Span<Arr> f) {
+		return cat.target(f.right);
+	}
+
+	@Override
+	public String showOb(Ob o) {
+	return o.toString();
+	}
+
+	@Override
+	public String showArr(Span<Arr> f) {
+	return f.toString();
+	}
+
+
+
 }
