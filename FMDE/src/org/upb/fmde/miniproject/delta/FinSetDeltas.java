425065bcf6d0dffbe27037ee261778f0db759396
Prenglis <j.e@vollbio.de>
2018-01-19 13:22:04 +0100

    test FinsetDelta

--- FMDE/src/org/upb/fmde/miniproject/delta/FinSetDeltas.java
+++ FMDE/src/org/upb/fmde/miniproject/delta/FinSetDeltas.java
@@ -1,10 +1,16 @@
 package org.upb.fmde.miniproject.delta;
 
+import static org.junit.Assert.assertFalse;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.upb.fmde.de.categories.colimits.pushouts.Span;
 import org.upb.fmde.de.categories.concrete.finsets.FinSet;
 import org.upb.fmde.de.categories.concrete.finsets.FinSetDiagram;
+import org.upb.fmde.de.categories.concrete.finsets.FinSets;
 import org.upb.fmde.de.categories.concrete.finsets.TotalFunction;
+import org.upb.fmde.de.categories.diagrams.Diagram;
+import org.upb.fmde.de.categories.limits.Limit;
 import org.upb.fmde.de.tests.TestUtil;
 
 public class FinSetDeltas<FinSet, TotalFunction> extends DeltaCategory<FinSet, TotalFunction> {
@@ -20,7 +26,37 @@ public class FinSetDeltas<FinSet, TotalFunction> extends DeltaCategory<FinSet, T
 	}
 	
 	@Test
+	public void testDeltaFinSet1() {
+		Diagram<FinSet, TotalFunction> d1 = createDiagram1();
+		FinSetsWithPullbacks fwp = new FinSetsWithPullbacks();
+		Limit<Span<TotalFunction>, TotalFunction> product =  fwp.product(d1.getObject("X"), d1.getObject("Z"));
+		
+		System.out.println("product result");
+		product.obj.left.src().elts().stream().forEach( (element) 
+				-> {
+					System.out.println(element);
+				});
+		System.out.println("left");
+		printMap(product.obj.left.mappings());
+		
+		System.out.println("right");
+		printMap(product.obj.right.mappings());
+		
+		
+Diagram<FinSet, TotalFunction> diagToShow = new FinSetDiagram();
+		
+		diagToShow.objects(diag.getObject("G"),diag.getObject("R"), product.obj.left.trg(), candidate).arrows(product.obj.left,product.obj.right, candidateToG, candidateToR, univCandidateToProduct, FinSets.FinSets.id(candidate));
+		
+		
+		
+		
+		diagToShow.saveAsDot(diagrams, "idcheck")
+		 .prettyPrint(diagrams, "idcheck");
+		
 	
+		assertFalse(product.obj.left.src().elts().isEmpty());
+		
+	}
 	private FinSetDiagram createDiagram1() {
 		FinSet X = new FinSet("X", "a", "b", "c");
 		FinSet Y = new FinSet("Y", 1, 2, "blup");
